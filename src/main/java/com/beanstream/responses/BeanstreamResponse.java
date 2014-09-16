a270c7fd3d8ef3445bf04444ae8a4e01cd4d3f85
pagarciaortega <pgarciaortega@gmail.com>
2014-09-16 09:37:46 -0400

    adding missing hashcode/equals implementation and missing @Test

--- src/main/java/com/beanstream/responses/BeanstreamResponse.java
+++ src/main/java/com/beanstream/responses/BeanstreamResponse.java
@@ -36,4 +36,33 @@ public class BeanstreamResponse {
 
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((category == null) ? 0 : category.hashCode());
+		result = prime * result + code;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BeanstreamResponse other = (BeanstreamResponse) obj;
+		if (category == null) {
+			if (other.category != null)
+				return false;
+		} else if (!category.equals(other.category))
+			return false;
+		if (code != other.code)
+			return false;
+		return true;
+	}
+
 }
--- src/test/java/com/beanstream/api/test/PaymentAPITest.java
+++ src/test/java/com/beanstream/api/test/PaymentAPITest.java
@@ -36,6 +36,7 @@ public class PaymentAPITest {
 		}
 	}
 
+	@Test
 	public void preAuthCompletionGreaterAmount() throws BeanstreamApiException {
 		CardPaymentRequest paymentRequest = getCreditCardPaymentRequest(
 				getRandomOrderId("GAS"), "300200578", "120.00");
